7ca2dbef3024300214f69d9825a776c6e7d5dd8b
huangzhi <601661706@qq.com>
2023-07-12 08:21:40 +0800

    feat: release

--- scripts/release.ts
+++ scripts/release.ts
@@ -7,6 +7,14 @@ import { PATHS } from './.internal/constants';
 import { assert, eachPkg, getPkgs } from './.internal/utils';
 
 (async () => {
+
+  const args = process.argv.slice(2);
+  let isDebug = false;
+  if (args && args.length) {
+    // debug模式下，不进行 git push tag、npm publish
+    isDebug = args.includes('debug') || args.includes('--debug');
+  }
+
   const { branch } = getGitRepoInfo();
   logger.info(`branch: ${branch}`);
   const pkgs = getPkgs();
@@ -84,53 +92,28 @@ import { assert, eachPkg, getPkgs } from './.internal/utils';
     await $`git tag v${version}`;
   }
 
-  // git push todo 网络好时，可以放开
-  // logger.event('git push');
-  // await $`git push origin ${branch} --tags`;
+  if (!isDebug) {
+    // logger.event('git push');
+    await $`git push origin ${branch} --tags`;
+  }
 
   // npm publish
   logger.event('pnpm publish');
   $.verbose = false;
   const innerPkgs = pkgs;
 
-  // check 2fa config
-  let otpArg: string[] = [];
-  // if (
-  //   (await $`npm profile get "two-factor auth"`).toString().includes('writes')
-  // ) {
-  //   let code = '';
-  //   do {
-  //     // get otp from user
-  //     code = await question('This operation requires a one-time password: ');
-  //     // generate arg for zx command
-  //     // why use array? https://github.com/google/zx/blob/main/docs/quotes.md
-  //     otpArg = ['--otp', code];
-  //   } while (code.length !== 6);
-  // }
 
   await Promise.all(
     innerPkgs.map(async (pkg) => {
-      await $`cd packages/${pkg} && npm publish --tag ${tag}`;
-      logger.info(`+ ${pkg}`);
+      if (!isDebug) {
+        await $`cd packages/${pkg} && npm publish --tag ${tag}`;
+        logger.info(`+ ${pkg}`);
+      }
     }),
   );
-  // await $`cd packages/umi && npm publish --tag ${tag} ${otpArg}`;
-  // logger.info(`+ umi`);
-  // await $`cd packages/max && npm publish --tag ${tag} ${otpArg}`;
-  // logger.info(`+ @umijs/max`);
+
   $.verbose = true;
 
-  // sync tnpm
-  // logger.event('sync tnpm');
-  $.verbose = false;
-  // await Promise.all(
-  //   pkgs.map(async (pkg) => {
-  //     const { name } = require(path.join(PATHS.PACKAGES, pkg, 'package.json'));
-  //     logger.info(`sync ${name}`);
-  //     await $`tnpm sync ${name}`;
-  //   }),
-  // );
-  $.verbose = true;
 })();
 
 function setDepsVersion(opts: {
